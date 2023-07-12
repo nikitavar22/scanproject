6e1047da519b575685cf2337a45fe47d72e3db8a
nikitavar22 <aleroev2000@list.ru>
2023-07-12 19:31:21 +0800

    last

--- src/view/App.js
+++ src/view/App.js
@@ -11,7 +11,7 @@ function App () {
     return (
         <Layout>
             <Search/>
-            <SimpleSlider/>
+            <Carousel/>
             <img src={imgHome}/>
             <Rates/>
         </Layout>
